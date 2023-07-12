c531742ac14fcb0bb38b5f3fb5b38b82028fafc9
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-07-12 21:25:12 +0100

    Finished basic look of the calculator.

--- js/calculator.js
+++ js/calculator.js
@@ -3,9 +3,9 @@ const screenText = document.querySelector(".screen-text");
 
 buttons.forEach((button) => button.addEventListener("click", function (e) {
     console.log(e);
-    let numberPressed = e.target.id;
-    console.log(numberPressed);
-    screenText.textContent = `${numberPressed}`;
+    let buttonPressed = e.target.id;
+    console.log(buttonPressed);
+    screenText.textContent = `${buttonPressed}`;
 }));
 
 buttons.forEach((button) => button.addEventListener("mouseover", function (e) {
@@ -15,3 +15,31 @@ buttons.forEach((button) => button.addEventListener("mouseover", function (e) {
 buttons.forEach((button) => button.addEventListener("mouseout", function (e) {
     button.classList.remove("button-hover");
 }));
+
+let add = function(a, b) {
+    return Number(a) + Number(b);
+}
+
+let subtract = function(a, b) {
+    return Number(a) - Number(b);
+}
+
+let multiply = function(a, b) {
+    return Number(a) * Number(b);
+}
+
+let divide = function(a, b) {
+    return Number(a) / Number(b)
+}
+
+let operate = function(a, b, op) {
+    if (op = "+") {
+        add(a,b);
+    } else if (op = "-") {
+        subtract(a, b);
+    } else if (op = "*") {
+        multiply(a, b);
+    } else {
+        divide(a, b);
+    }
+}
