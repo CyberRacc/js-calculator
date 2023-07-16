b8db5cce1e58e7708416878fe8d0ead241689d74
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-07-16 10:55:02 +0100

    Added functioning keyboard support and notes explaning certain bits of code.

--- js/calculator.js
+++ js/calculator.js
@@ -100,6 +100,19 @@ let eventListeners = function() {
         deleteButton.classList.remove("button-hover");
         deleteButton.classList.remove("button-click");
     });
+
+    clearButton.addEventListener("mousedown", e => {
+        clearButton.classList.add("button-click");
+    });
+    
+    clearButton.addEventListener("mouseup", e => {
+        clearButton.classList.remove("button-click");
+    });
+    
+    clearButton.addEventListener("mouseout", e => {
+        clearButton.classList.remove("button-hover");
+        clearButton.classList.remove("button-click");
+    });
     
     equalsButton.addEventListener("click", e => {
         if (currentNum && currentOp && nextNum) {
@@ -120,6 +133,71 @@ let eventListeners = function() {
         currentOp = null;
         screenText.textContent = screenData;
     });
+
+    window.addEventListener('keydown', e => {
+        let key = e.key;
+    
+        // If the key pressed was a number or a decimal point
+        if (!isNaN(key) || key === '.') {
+            // Check if a decimal point is already present in the current number or the next number
+            if (key === "." && ((currentOp === null && currentNum.includes(".")) || (currentOp !== null && nextNum.includes(".")))) {
+                return;
+            }
+    
+            // If no operation has been chosen yet, append the key to the current number
+            if (currentOp === null) {
+                currentNum += key;
+            } else { // If an operation has been chosen, append the key to the next number
+                nextNum += key;
+            }
+        } else if (key === "+" || key === "-" || key === "*" || key === "/") { // If the key pressed was an operator
+            if (key === "/") key = "÷"; // Translate / into division symbol
+    
+            if (currentNum && currentOp && nextNum) {
+                currentNum = operate();
+                nextNum = "";
+            }
+            if (currentNum) {
+                currentOp = key;
+            }
+        } else if (key === "Enter" || key === "=") { // If the equals key or the enter key was pressed
+            if (currentNum && currentOp && nextNum) {
+                currentNum = operate();
+                nextNum = "";
+                currentOp = null;
+            } else if (currentNum && currentOp) {
+                currentNum = operate();
+                currentOp = null;
+            }
+        } else if (key === "Backspace") { // If the backspace key was pressed
+            // Remove the last character from screenData, currentNum, and nextNum accordingly
+            if (nextNum) {
+                nextNum = nextNum.slice(0, -1);
+            } else if (currentOp) {
+                currentOp = null;
+            } else if (currentNum) {
+                currentNum = currentNum.slice(0, -1);
+            }
+            screenData = currentNum + (currentOp || '') + nextNum;
+            screenText.textContent = screenData;
+        } else if (key === "Escape") { // If the escape key was pressed
+            screenData = "";
+            currentNum = "";
+            nextNum = "";
+            currentOp = null;
+            screenText.textContent = screenData;
+        }
+    
+        // Update the screen data
+        if (currentNum.length + nextNum.length < 16) {
+            screenData = currentNum + (currentOp || '') + nextNum;
+            screenText.textContent = screenData;
+        }
+    });
+    
+    
+    
+    
 }
 
 let add = function(a, b) {
@@ -146,11 +224,7 @@ let operate = function() {
 
     let result;
 
-    if (!num1 || !operator || !num2) {
-        return;
-    }
-
-    if (num1 === null || num1 === undefined || operator === null || operator === undefined || num2 === null || num2 === undefined) {
+    if (num1 === "" || operator === "" || num2 === "") {
         return;
     }
 
@@ -169,12 +243,23 @@ let operate = function() {
                 result = divide(num1, num2);
             } else {
                 alert("You cannot divide by zero!");
-                return;
+                currentNum = "";
+                nextNum = "";
+                currentOp = null;
+                screenData = "";
+                screenText.textContent = "";
+                return "";
             }
             break;
         default:
             return; // if the operator is not known, exit the function
     }
+
+    if (result.toString().length > 16) {
+        result = result.toFixed( 16 - (Math.floor(result) + "").length); // If result is too long, reduce decimal part to fit.
+        result = parseFloat(result); // Removes traling zeros.
+    }
+
     screenData = result.toString(); // Convert the result to a string to display.
     screenText.textContent = screenData; // update the screen with the result
 
